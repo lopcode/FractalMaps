6fc93fee2abcbef5a319d63e18112f5382e69033
Sky <git@bunnies.io>
2014-10-22 22:11:41 +0100

    Use ButterKnife to make DetailControl a little more sane

--- MandelbrotMapsAndroid/app/src/main/java/uk/ac/ed/inf/mandelbrotmaps/DetailControl.java
+++ MandelbrotMapsAndroid/app/src/main/java/uk/ac/ed/inf/mandelbrotmaps/DetailControl.java
@@ -12,15 +12,30 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class DetailControl extends Activity implements OnClickListener, OnSeekBarChangeListener {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class DetailControl extends Activity implements OnSeekBarChangeListener {
+    @InjectView(R.id.detail_apply_button)
     Button applyButton;
+
+    @InjectView(R.id.default_detail_button)
     Button defaultsButton;
+
+    @InjectView(R.id.detail_cancel_button)
     Button cancelButton;
 
+    @InjectView(R.id.mandelbrot_seekbar)
     SeekBar mandelbrotBar;
+
+    @InjectView(R.id.julia_seekbar)
     SeekBar juliaBar;
 
+    @InjectView(R.id.mandelbrotText)
     TextView mandelbrotText;
+
+    @InjectView(R.id.juliaText)
     TextView juliaText;
 
     boolean changed = false;
@@ -29,51 +44,41 @@ public class DetailControl extends Activity implements OnClickListener, OnSeekBa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailcontrol);
-
-        // Get references to UI elements
-        applyButton = (Button) findViewById(R.id.detail_apply_button);
-        applyButton.setOnClickListener(this);
-        defaultsButton = (Button) findViewById(R.id.default_detail_button);
-        defaultsButton.setOnClickListener(this);
-        cancelButton = (Button) findViewById(R.id.detail_cancel_button);
-        cancelButton.setOnClickListener(this);
-
-        // Assign TextViews before their values are set when the SeekBars change
-        mandelbrotText = (TextView) findViewById(R.id.mandelbrotText);
-        juliaText = (TextView) findViewById(R.id.juliaText);
+        ButterKnife.inject(this);
 
         // Get references to SeekBars, set their value from the prefs
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-        mandelbrotBar = (SeekBar) findViewById(R.id.mandelbrot_seekbar);
         mandelbrotBar.setOnSeekBarChangeListener(this);
         mandelbrotBar.setProgress((int) prefs.getFloat(FractalActivity.mandelbrotDetailKey, (float) AbstractFractalView.DEFAULT_DETAIL_LEVEL));
 
-        juliaBar = (SeekBar) findViewById(R.id.julia_seekbar);
         juliaBar.setOnSeekBarChangeListener(this);
         juliaBar.setProgress((int) prefs.getFloat(FractalActivity.juliaDetailKey, (float) AbstractFractalView.DEFAULT_DETAIL_LEVEL));
     }
 
-    public void onClick(View view) {
-        int button = view.getId();
+    @OnClick(R.id.detail_cancel_button)
+    public void onDetailCancelButtonClicked() {
+        this.finish();
+    }
 
-        if (button == R.id.detail_cancel_button) {
-            finish();
-        } else if (button == R.id.default_detail_button) {
-            juliaBar.setProgress((int) AbstractFractalView.DEFAULT_DETAIL_LEVEL);
-            mandelbrotBar.setProgress((int) AbstractFractalView.DEFAULT_DETAIL_LEVEL);
-        } else if (button == R.id.detail_apply_button) {
-            //Set shared prefs and return value (to indicate if shared prefs have changed)
-            SharedPreferences.Editor prefsEditor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
-            prefsEditor.putFloat(FractalActivity.mandelbrotDetailKey, (float) mandelbrotBar.getProgress());
-            prefsEditor.putFloat(FractalActivity.juliaDetailKey, (float) juliaBar.getProgress());
+    @OnClick(R.id.default_detail_button)
+    public void onDefaultDetailButtonClicked() {
+        juliaBar.setProgress((int) AbstractFractalView.DEFAULT_DETAIL_LEVEL);
+        mandelbrotBar.setProgress((int) AbstractFractalView.DEFAULT_DETAIL_LEVEL);
+    }
 
-            prefsEditor.commit();
+    @OnClick(R.id.detail_apply_button)
+    public void onDetailApplyButtonClicked() {
+        //Set shared prefs and return value (to indicate if shared prefs have changed)
+        SharedPreferences.Editor prefsEditor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
+        prefsEditor.putFloat(FractalActivity.mandelbrotDetailKey, (float) mandelbrotBar.getProgress());
+        prefsEditor.putFloat(FractalActivity.juliaDetailKey, (float) juliaBar.getProgress());
 
-            changed = true;
+        prefsEditor.commit();
 
-            finish();
-        }
+        changed = true;
+
+        finish();
     }
 
     @Override
